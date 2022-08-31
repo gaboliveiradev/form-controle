ccd7d5ba8ecbe8b82d687dbf9b1ae46fa9d2abc6
gaboliveiradev <g38789138@gmail.com>
2022-08-31 10:44:27 -0300

    CRIANDO A CLASSE DE CONEXÃO COM O MYSQL

--- DAL/connMySQL.cs
+++ DAL/connMySQL.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MySql.Data.MySqlClient;
+
 namespace FormControle.DAL
 {
     internal class connMySQL
     {
+        private MySqlConnection connection;
+
+        private string server = "";
+        private string port = "";
+        private string dbname = "";
+        private string uid = "";
+        private string pwd = "";
+
+        public connMySQL()
+        {
+            string dsn = $"server={server};Port={port};database={dbname};Uid={uid};Pwd={pwd}";
+            connection = new MySqlConnection(dsn);
+        }
+
+        public MySqlConnection toConnect()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed) connection.Open();
+            return connection;
+        }
+
+        public void disconnect()
+        {
+            if(connection.State == System.Data.ConnectionState.Open) connection.Close();
+        }
     }
 }
