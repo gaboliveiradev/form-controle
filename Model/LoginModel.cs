29c9981700cffd58084eee9d6e1b01d69d34158b
gaboliveiradev <g38789138@gmail.com>
2022-09-13 15:45:03 -0300

    build do metodo de autenticar login da LoginModel

--- Model/LoginModel.cs
+++ Model/LoginModel.cs
@@ -5,13 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 using MySql.Data.MySqlClient;
+using FormControle.DAL;
+using FormControle.View.Modules.Principal;
 
 namespace FormControle.Model
 {
     internal class LoginModel
     {
-        public string email, senha;
+        public string id, email, senha;
         public MySqlDataReader dr;
+
+        public bool Authenticate()
+        {
+            LoginDAL dal = new LoginDAL();
+            dr = dal.selectByUserForLogin(this);
+
+            while(dr.Read())
+            {
+                id = dr.GetString("id");
+                email = dr.GetString("email");
+            }
+
+            FormPrincipal principal = new FormPrincipal();
+            if (id != null)
+            {
+                principal.Show();
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
 
     }
 }
