d40cf929479e014e724c4cf738781021a67db557
gaboliveiradev <g38789138@gmail.com>
2022-09-13 22:50:02 -0300

    criando metodos de marcar, desmarcar, importar, excluir e aplicar percentual

--- View/Modules/Principal/FormPrincipal.Designer.cs
+++ View/Modules/Principal/FormPrincipal.Designer.cs
@@ -28,21 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgv_produto = new System.Windows.Forms.DataGridView();
+            this.btn_importar = new System.Windows.Forms.Button();
+            this.btn_marcar_desmarcar = new System.Windows.Forms.Button();
+            this.btn_excluir = new System.Windows.Forms.Button();
+            this.btn_aplicar_percentual = new System.Windows.Forms.Button();
+            this.ofd_arquivo = new System.Windows.Forms.OpenFileDialog();
+            this.cln_checkbox = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.clm_ean = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cln_nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cln_valor_compra = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cln_valor_venda = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cln_estoque = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txt_percentual = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_produto)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgv_produto
+            // 
+            this.dgv_produto.AllowUserToAddRows = false;
+            this.dgv_produto.AllowUserToOrderColumns = true;
+            this.dgv_produto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_produto.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cln_checkbox,
+            this.clm_ean,
+            this.cln_nome,
+            this.cln_valor_compra,
+            this.cln_valor_venda,
+            this.cln_estoque});
+            this.dgv_produto.Location = new System.Drawing.Point(12, 12);
+            this.dgv_produto.Name = "dgv_produto";
+            this.dgv_produto.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_produto.Size = new System.Drawing.Size(1056, 545);
+            this.dgv_produto.TabIndex = 0;
+            this.dgv_produto.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_produto_CellClick);
+            // 
+            // btn_importar
+            // 
+            this.btn_importar.Location = new System.Drawing.Point(12, 567);
+            this.btn_importar.Name = "btn_importar";
+            this.btn_importar.Size = new System.Drawing.Size(135, 36);
+            this.btn_importar.TabIndex = 1;
+            this.btn_importar.Text = "Importar Arquivo";
+            this.btn_importar.UseVisualStyleBackColor = true;
+            this.btn_importar.Click += new System.EventHandler(this.btn_importar_Click);
+            // 
+            // btn_marcar_desmarcar
+            // 
+            this.btn_marcar_desmarcar.Location = new System.Drawing.Point(153, 567);
+            this.btn_marcar_desmarcar.Name = "btn_marcar_desmarcar";
+            this.btn_marcar_desmarcar.Size = new System.Drawing.Size(135, 36);
+            this.btn_marcar_desmarcar.TabIndex = 2;
+            this.btn_marcar_desmarcar.Text = "Marcar Todos";
+            this.btn_marcar_desmarcar.UseVisualStyleBackColor = true;
+            this.btn_marcar_desmarcar.Click += new System.EventHandler(this.btn_marcar_desmarcar_Click);
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.Location = new System.Drawing.Point(294, 567);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(135, 36);
+            this.btn_excluir.TabIndex = 3;
+            this.btn_excluir.Text = "Excluir";
+            this.btn_excluir.UseVisualStyleBackColor = true;
+            this.btn_excluir.Click += new System.EventHandler(this.btn_excluir_Click);
+            // 
+            // btn_aplicar_percentual
+            // 
+            this.btn_aplicar_percentual.Location = new System.Drawing.Point(933, 567);
+            this.btn_aplicar_percentual.Name = "btn_aplicar_percentual";
+            this.btn_aplicar_percentual.Size = new System.Drawing.Size(135, 36);
+            this.btn_aplicar_percentual.TabIndex = 5;
+            this.btn_aplicar_percentual.Text = "Aplicar";
+            this.btn_aplicar_percentual.UseVisualStyleBackColor = true;
+            this.btn_aplicar_percentual.Click += new System.EventHandler(this.btn_aplicar_percentual_Click);
+            // 
+            // cln_checkbox
+            // 
+            this.cln_checkbox.HeaderText = "";
+            this.cln_checkbox.Name = "cln_checkbox";
+            // 
+            // clm_ean
+            // 
+            this.clm_ean.HeaderText = "EAN";
+            this.clm_ean.Name = "clm_ean";
+            // 
+            // cln_nome
+            // 
+            this.cln_nome.HeaderText = "NOME";
+            this.cln_nome.Name = "cln_nome";
+            // 
+            // cln_valor_compra
+            // 
+            this.cln_valor_compra.HeaderText = "VALOR COMPRA";
+            this.cln_valor_compra.Name = "cln_valor_compra";
+            // 
+            // cln_valor_venda
+            // 
+            this.cln_valor_venda.HeaderText = "VALOR_VENDA";
+            this.cln_valor_venda.Name = "cln_valor_venda";
+            // 
+            // cln_estoque
+            // 
+            this.cln_estoque.HeaderText = "ESTOQUE";
+            this.cln_estoque.Name = "cln_estoque";
+            // 
+            // txt_percentual
+            // 
+            this.txt_percentual.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_percentual.Location = new System.Drawing.Point(792, 568);
+            this.txt_percentual.Name = "txt_percentual";
+            this.txt_percentual.Size = new System.Drawing.Size(135, 33);
+            this.txt_percentual.TabIndex = 6;
             // 
             // frm_principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1084, 611);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Controls.Add(this.txt_percentual);
+            this.Controls.Add(this.btn_aplicar_percentual);
+            this.Controls.Add(this.btn_excluir);
+            this.Controls.Add(this.btn_marcar_desmarcar);
+            this.Controls.Add(this.btn_importar);
+            this.Controls.Add(this.dgv_produto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frm_principal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormPrincipal";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_produto)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgv_produto;
+        private System.Windows.Forms.Button btn_importar;
+        private System.Windows.Forms.Button btn_marcar_desmarcar;
+        private System.Windows.Forms.Button btn_excluir;
+        private System.Windows.Forms.Button btn_aplicar_percentual;
+        private System.Windows.Forms.OpenFileDialog ofd_arquivo;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn cln_checkbox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clm_ean;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cln_nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cln_valor_compra;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cln_valor_venda;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cln_estoque;
+        private System.Windows.Forms.TextBox txt_percentual;
     }
 }
--- View/Modules/Principal/FormPrincipal.cs
+++ View/Modules/Principal/FormPrincipal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,75 @@ namespace FormControle.View.Modules.Principal
         public frm_principal()
         {
             InitializeComponent();
+            dgv_produto.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void btn_importar_Click(object sender, EventArgs e)
+        {
+            dgv_produto.RowCount = 0;
+
+            ofd_arquivo.FileName = "";
+            ofd_arquivo.ShowDialog();
+            StreamReader arq = File.OpenText(ofd_arquivo.FileName);
+            string linha;
+
+            while((linha = arq.ReadLine()) != null)
+            {
+                string[] dados = linha.Split(';');
+                string ean = dados[0];
+                string nome = dados[1];
+                string valor_compra = dados[2];
+                string valor_venda = dados[3];
+                string estoque = dados[4];
+
+                dgv_produto.Rows.Add(false, ean, nome, valor_compra, valor_venda, estoque);
+            }
+        }
+
+        private void btn_marcar_desmarcar_Click(object sender, EventArgs e)
+        {
+            if(btn_marcar_desmarcar.Text == "Marcar Todos")
+            {
+                foreach(DataGridViewRow l in dgv_produto.Rows)
+                    l.Cells[0].Value = true;
+
+                btn_marcar_desmarcar.Text = "Desmarcar Todos";
+            } else
+            {
+                foreach (DataGridViewRow l in dgv_produto.Rows)
+                    l.Cells[0].Value = false;
+
+                btn_marcar_desmarcar.Text = "Marcar Todos";
+            }
+        }
+
+        private void dgv_produto_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dgv_produto.CurrentRow.Cells[0].Value = (Convert.ToBoolean(dgv_produto.CurrentRow.Cells[0].Value) == false) ? true : false;
+        }
+
+        private void btn_excluir_Click(object sender, EventArgs e)
+        {
+            for (int i = dgv_produto.RowCount - 1; i >= 0; i--)
+            {
+                if (Convert.ToBoolean(dgv_produto.CurrentRow.Cells[0].Value) == true)
+                {
+                    dgv_produto.Rows.Remove(dgv_produto.Rows[i]);
+                }
+            }
+        }
+
+        private void btn_aplicar_percentual_Click(object sender, EventArgs e)
+        {
+            if(Convert.ToBoolean(dgv_produto.CurrentRow.Cells[0].Value) == true) {
+                double percentual = Convert.ToDouble(dgv_produto.CurrentRow.Cells[4].Value) * double.Parse(txt_percentual.Text.ToString());
+                double valor_atual = double.Parse(dgv_produto.CurrentRow.Cells[4].Value.ToString());
+                double valor_final = valor_atual + percentual;
+
+                Console.WriteLine($"Percentual: {percentual} | Valor Atual: {valor_atual} | Valor Final: {valor_final}");
+
+                dgv_produto.CurrentRow.Cells[4].Value = valor_final.ToString();
+            }
         }
     }
 }
